3581ddc887235bffa534ba8301c9d4ee54223a31
vanshaggrwal <101628876+vanshaggrwal@users.noreply.github.com>
2025-05-04 12:41:00 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-
+import '../styles/login.css';
+import '../styles/register.css';
+import '../styles/Footer.css';
 
 export default function App({ Component, pageProps }) {
   return (
